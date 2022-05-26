ac06890e25ce4a8110e29a510317ce4ba546d494
hasanceviz <65962794+hasanceviz@users.noreply.github.com>
2022-05-26 20:28:07 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace onetimepassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.text_Mail = new DevExpress.XtraEditors.TextEdit();
+            this.text_Kod = new DevExpress.XtraEditors.TextEdit();
+            this.buton_Kodİste = new DevExpress.XtraEditors.SimpleButton();
+            this.buton_GirisYap = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.text_Mail.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.text_Kod.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(205, 114);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(48, 16);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "E-Mail  :";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(205, 223);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(92, 16);
+            this.labelControl3.TabIndex = 2;
+            this.labelControl3.Text = "Güvenlik Kodu  :";
+            // 
+            // text_Mail
+            // 
+            this.text_Mail.Location = new System.Drawing.Point(406, 111);
+            this.text_Mail.Name = "text_Mail";
+            this.text_Mail.Size = new System.Drawing.Size(125, 22);
+            this.text_Mail.TabIndex = 3;
+            // 
+            // text_Kod
+            // 
+            this.text_Kod.Location = new System.Drawing.Point(406, 217);
+            this.text_Kod.Name = "text_Kod";
+            this.text_Kod.Size = new System.Drawing.Size(125, 22);
+            this.text_Kod.TabIndex = 5;
+            // 
+            // buton_Kodİste
+            // 
+            this.buton_Kodİste.Location = new System.Drawing.Point(422, 158);
+            this.buton_Kodİste.Name = "buton_Kodİste";
+            this.buton_Kodİste.Size = new System.Drawing.Size(94, 29);
+            this.buton_Kodİste.TabIndex = 6;
+            this.buton_Kodİste.Text = "Kod İste";
+            this.buton_Kodİste.Click += new System.EventHandler(this.buton_Giris_Click);
+            // 
+            // buton_GirisYap
+            // 
+            this.buton_GirisYap.Location = new System.Drawing.Point(422, 263);
+            this.buton_GirisYap.Name = "buton_GirisYap";
+            this.buton_GirisYap.Size = new System.Drawing.Size(94, 29);
+            this.buton_GirisYap.TabIndex = 7;
+            this.buton_GirisYap.Text = "Giriş Yap";
+            this.buton_GirisYap.Click += new System.EventHandler(this.buton_GirisYap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buton_GirisYap);
+            this.Controls.Add(this.buton_Kodİste);
+            this.Controls.Add(this.text_Kod);
+            this.Controls.Add(this.text_Mail);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.text_Mail.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.text_Kod.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.TextEdit text_Mail;
+        private DevExpress.XtraEditors.TextEdit text_Kod;
+        private DevExpress.XtraEditors.SimpleButton buton_Kodİste;
+        private DevExpress.XtraEditors.SimpleButton buton_GirisYap;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Mail;
+
+namespace onetimepassword
+{
+    public partial class Form1 : Form
+    {
+        public int Gkod;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buton_Giris_Click(object sender, EventArgs e)
+        {
+            if (text_Mail.Text == "" )
+            {
+                MessageBox.Show("Mail Boş Olamaz.");
+            }
+            else {
+                Random rnd = new  Random();
+                string konu = "Güvenlik Doğrulaması : ";
+                int sayi = rnd.Next(1000, 9999);
+                Gkod = sayi;
+                Gonder(konu, sayi);
+            }
+        }
+        public bool Gonder (string konu, int sayi)
+        {
+            string icerik = "Güvenlik Doğrulaması :" + sayi;
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("hasancvzz@outlook.com");
+            mail.To.Add(text_Mail.Text);
+            mail.Subject = konu;
+            mail.Body =icerik;
+            SmtpClient sc = new SmtpClient();
+            sc.Credentials = new System.Net.NetworkCredential("hasancvzz@outlook.com", "1234056789Aa.");
+            sc.Port = 587;
+            sc.Host = "smtp.live.com";
+            sc.EnableSsl = true;
+            object userState = mail;
+            bool kontrol = true;
+            try
+            {
+                sc.SendAsync(mail, (object)mail);
+            }
+            catch (SmtpException ex)
+            {
+                kontrol = false;
+                System.Windows.Forms.MessageBox.Show(ex.Message, "Mail Gönderme Hatasi");
+            }
+            return kontrol;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        
+
+        private void buton_GirisYap_Click(object sender, EventArgs e)
+        {
+            if(Gkod == Convert.ToInt32(text_Kod.Text))
+            {
+                MessageBox.Show("giriş başarılı");
+                this.Hide();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("girilen kod hatalıdır.");
+            }
+
+
+        }
+        
+
+        }
+    }
+
